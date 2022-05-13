20152ab54d69ba70594eb58ff7409fbf62d03be4
PeterFalkTINF20B2 <85633848+PeterFalkTINF20B2@users.noreply.github.com>
2022-05-13 18:03:54 +0200

    Transfer method amoutFor()
    Transfer method amountFor() from class Customer to Rental
    Rename transfered methdo to getCharge()

--- src/Customer.java
+++ src/Customer.java
@@ -47,23 +47,6 @@ class Customer {
 	}
 
 	private double amountFor(Rental aRental) {
-		double amount = 0;
-		switch (aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			amount += 2;
-			if (aRental.getDaysRented() > 2)
-				amount += (aRental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			amount += aRental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			amount += 1.5;
-			if (aRental.getDaysRented() > 3)
-				amount += (aRental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return amount;
+		return aRental.getCharge();
 	}
-
 }
--- src/Rental.java
+++ src/Rental.java
@@ -1,14 +1,37 @@
 class Rental {
-    private Movie movie;
-    private int daysRented;
-    public Rental(Movie newmovie, int newdaysRented) {
-        movie = newmovie;
-        daysRented = newdaysRented;
-    }
-    public int getDaysRented() {
-        return daysRented;
-    }
-    public Movie getMovie() {
-        return movie;
-    }
+	private Movie movie;
+	private int daysRented;
+
+	public Rental(Movie newmovie, int newdaysRented) {
+		movie = newmovie;
+		daysRented = newdaysRented;
+	}
+
+	public int getDaysRented() {
+		return daysRented;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public double getCharge() {
+		double amount = 0;
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			amount += 2;
+			if (getDaysRented() > 2)
+				amount += (getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			amount += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			amount += 1.5;
+			if (getDaysRented() > 3)
+				amount += (getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return amount;
+	}
 }
